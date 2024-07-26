dc053f8c75de1271bf4ee0cf03a737c2b8d024c5
Silpharny <silpharny@gmail.com>
2024-07-26 19:55:04 -0300

    Atualizando Branch de dev

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react"
-import { Container } from "./styles"
+import { Container, Text } from "./styles"
+import { auth, db } from "../../services/firebaseConfig"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 
 export default function Login() {
   const [isRegister, setIsRegister] = useState(false)
 
+  function handleRegister() {
+    const newAuth = auth
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(newAuth, provider).then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
+      // The signed-in user info.
+      const user = result.user
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+  }
+
   if (isRegister) {
     return (
       <Container>
-        <h1>Register</h1>
+        <Text>Register</Text>
       </Container>
     )
   } else {
     return (
       <Container>
-        <h1>Login</h1>
+        <Text>Login</Text>
       </Container>
     )
   }
